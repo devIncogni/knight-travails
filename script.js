d8558a4ce471444626a1d88b1e55d642611af0d8
devIncogni <arinkumar10@outlook.com>
2025-06-28 19:39:14 +0530

    Added some usable functions to cheesSquare class

--- script.js
+++ script.js
@@ -2,8 +2,24 @@ class ChessSquare {
   constructor(i, j) {
     this.currentLocation = [i, j];
     this.discovered = false;
-    this.previousSquare = null;
+    this.previousSquare = [null];
     this.shortestDistance = null;
+  }
+
+  isDiscoverd() {
+    return this.discovered;
+  }
+
+  currShortestDist() {
+    return this.shortestDistance;
+  }
+
+  currLocationString() {
+    return this.currentLocation.join("");
+  }
+
+  prevLocationString() {
+    return this.previousSquare.join("");
   }
 }
 
@@ -12,5 +28,16 @@ class ChessBoard {
     this.board = Array.from({ length: 8 }, (_, i) =>
       Array.from({ length: 8 }, (_, j) => new ChessSquare(i, j))
     );
+
+    this.dx = [2, 2, -2, -2, 1, -1, 1, -1];
+    this.dy = [1, -1, 1, -1, 2, 2, -2, -2];
   }
+}
+
+class Knight {
+  constructor(chessBoard) {
+    this.chessBoard = chessBoard;
+  }
+
+  moveKnight(start = [0, 0], end = [2, 1]) {}
 }
