fac9d2ac1d678bc64a5e714beffe9c0cc6fb26e8
devIncogni <arinkumar10@outlook.com>
2025-06-28 20:04:55 +0530

    Wrote a function that returns an array of next possible squares

--- script.js
+++ script.js
@@ -28,16 +28,41 @@ class ChessBoard {
     this.board = Array.from({ length: 8 }, (_, i) =>
       Array.from({ length: 8 }, (_, j) => new ChessSquare(i, j))
     );
-
-    this.dx = [2, 2, -2, -2, 1, -1, 1, -1];
-    this.dy = [1, -1, 1, -1, 2, 2, -2, -2];
   }
 }
 
 class Knight {
   constructor(chessBoard) {
     this.chessBoard = chessBoard;
+
+    this.dx = [2, 2, -2, -2, 1, -1, 1, -1];
+    this.dy = [1, -1, 1, -1, 2, 2, -2, -2];
   }
 
-  moveKnight(start = [0, 0], end = [2, 1]) {}
+  nextMovableSquares(start) {
+    const nextSetOfMovableSquares = [];
+
+    for (let i = 0; i < 8; i++) {
+      const newX = start[0] + dx[i];
+      const newY = start[1] + dy[i];
+
+      if (newX >= 0 && newY >= 0 && newX < 8 && newY < 8) {
+        nextSetOfMovableSquares.push([newX, newY]);
+      }
+    }
+    return nextSetOfMovableSquares;
+  }
+
+  moveKnight(start = [0, 0], end = [2, 1]) {
+    if (start == end) {
+      return end;
+    }
+
+    const nextSetOfMovableSquares = this.nextMovableSquares(start);
+
+    for (let i = 0; i < nextSetOfMovableSquares.length; i++) {
+      const currentProbingSquare = nextSetOfMovableSquares[i];
+      
+    }
+  }
 }
